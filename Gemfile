2b42f3954f134245a108314cea81c4d735a44779
ChtobiUchitsyaNadoBitProfi <ivan.mielnikov.01@mail.ru>
2022-01-15 11:56:37 +0700

    add avtorization with devise gem

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'sass-rails', '>= 6'
 gem 'simple_form'
 gem 'turbolinks', '~> 5'
 gem 'webpacker', '~> 5.0'
+gem 'devise'
 
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :items do
     get :iv_821, on: :collection
     get :iv_822, on: :collection
     get :iv_823, on: :collection
-    get :admin, on: :collection
   end
 
   root to: 'items#iv_821'
